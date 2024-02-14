3584a3ba247e23c37ab5dfbb6378f0f7e2cf31ca
Mohammad Sawel <sawelmohammad6@gmail.com>
2024-02-15 03:42:12 +0600

    added playground and keyboard and js file

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,7 @@
+//function play(){
+//Step-1: Hide the home screen. to hide the screen add the class hidden to the home section
+//const homeSection = document.getElementById('home-screen');
+//homeSection.classList.add('hidden');
+//show the playground
+//const playgroundSection = document.getElementById('play-ground');
+//playgroundSection.classList.remove('hidden');}
